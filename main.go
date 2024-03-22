95de3af895034a963e04f682cdcc19be81e3c6b7
Davieas <davialexandresantana@gmail.com>
2024-03-22 09:51:54 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "github.com/Davieas/goapi-oportunites/router"
+import (
+	"github.com/Davieas/goapi-oportunites/config"
+	"github.com/Davieas/goapi-oportunites/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	//Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
 	router.Init()
 }
